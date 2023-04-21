054327513d42b012caf9a64d90207c2751c74f02
mazen <mazenhamada2003@gmail.com>
2023-04-21 12:04:18 -0700

    m

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<unistd.h>
+/**
+ * main -Entry point
+ *
+ * Description: print a quete using write function write (int fd,const void "buf",size_t count)
+ *
+ * Return:1 (not success)
+*/
+int main(void)
+{
+char que[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, que, 59);
+return (1);
+}
