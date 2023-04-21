46aa1f12f3113f7303e5a98a3e5f67f7eaaa4d66
mazen <mazenhamada2003@gmail.com>
2023-04-21 10:54:15 -0700

    m

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: A c program that prints with puts function
+ *
+ * Return: Alawys 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
